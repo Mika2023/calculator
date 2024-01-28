b4428d7068b5bb890e2ae5b4df46ed2cc43863a2
Mika2023 <123239316+Mika2023@users.noreply.github.com>
2024-01-28 07:49:22 +0000

    added functions and their priority,  please read comments

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,7 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
+#include <cstring>
+
 using namespace std;
-class Stack {
+
+
+class Stack
+{
     double arr[1000];
     int head, maxsize;
 public:
@@ -30,6 +35,13 @@ int priority(char sign)
 {
     switch (sign)
     {
+    case 's':
+    case 'c'://как котангенс считать?..
+    case 't':
+    case 'l':
+        return 4;
+    case '^':
+        return 3;
     case '*':
     case '/':
         return 2;
@@ -39,19 +51,31 @@ int priority(char sign)
     }
     return 0;
 }
-void polish_cow(char* expression)
-{
-    cout << "Expression:" << endl;
-    cout << expression << endl;
+
+void polish_cow(char* expression)//эта функция принимает строку с выражением (не надо) и преобразует в польскую нотацию (строку)
+{//надо заменить входную строку на чтение с потока ввода
+//может, польская нотация не нужна в виде строки именно...
     char polish[1000] = { 0 };
     int len = strlen(expression);
-    int a = 0;
+    int a = 0, f = 1;//флаг для унарного минуса (когда надо считать отрицательное число): 1 - минус унарный, 0 - обычный
     Stack signs;
     for (int i = 0; i < len; ++i) {
-        if (expression[i] >= '0' && expression[i] <= '9') polish[a++] = expression[i];
+        if (expression[i] >= '0' && expression[i] <= '9' || expression[i]=='-' && f || expression[i] == 'e' || expression[i] == 'E' || expression[i]=='p' || expression[i]=='P')
+        {
+          double number = 0;
+          if (expression[i] == 'e' || expression[i] == 'E') number = exp(1);
+          else if (expression[i] == 'p' || expression[i] == 'P') number = M_PI;
+          //это число либо е, либо пи, либо вводимое пользователем
+          polish[a++] = expression[i];
+          f = 0;
+        }
         else {
             if (a > 0 && polish[a - 1] != ' ') polish[a++] = ' ';
-            if (expression[i] == '(') signs.push('(');
+            if (expression[i] == '(')
+            {
+              signs.push('(');
+              f = 1;
+            }
             else if (expression[i] == ')')
             {
                 while (signs.get_top() != -1000000000 && signs.get_top() != '(')
@@ -79,9 +103,7 @@ void polish_cow(char* expression)
         polish[a++] = signs.pop();
         polish[a++] = ' ';
     }
-
-    cout << "Reverse Polish Notation:" << endl;
-    cout << polish << endl;
+  
     int res = 0;
     Stack nums;
     int i = 0;
@@ -107,6 +129,21 @@ void polish_cow(char* expression)
         case '-':
             res = -(nums.pop() - nums.pop());
             break;
+        case '^':
+            res = pow(nums.pop(), nums.pop());
+            break;
+          case 's':
+              res = sin(nums.pop());
+              break;
+          case 'c':
+              res = cos(nums.pop());
+              break;
+          case 't':
+              res = tan(nums.pop());
+              break;
+          case 'l':
+              res = log(nums.pop());
+              break;
         case '*':
             res = nums.pop() * nums.pop();
             break;
@@ -114,13 +151,14 @@ void polish_cow(char* expression)
             int n1 = nums.pop(), n2 = nums.pop();
             res = n2 / n1;
             break;
+        
         }
         nums.push(res);
         ++i;
     }
-    cout << "Result:" << endl;
     cout << nums.get_top();
 }
+
 int main()
 {
     
